cac87fc25f110a63f91233fc75959504b8446de9
zhdi <zhdidi325@gmail.com>
2025-09-19 13:07:27 +0800

    add chinese chars encode

--- lua/core/init.lua
+++ lua/core/init.lua
@@ -25,6 +25,7 @@ vim.opt.keywordprg            = 'man'
 vim.opt.scrolloff             = 8
 vim.opt.sidescrolloff         = 8
 vim.opt.undofile              = true
+vim.opt.fileencodings = 'ucs-bom,utf-8,cp936,gb18030,big5,euc-jp,euc-kr,latin1'
 
 -- fold
 vim.o.foldcolumn              = '1'
